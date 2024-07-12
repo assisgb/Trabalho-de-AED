b31a1a9bcfd845b219410e6d297e3f827dca5377
assisgb <138792822+assisgb@users.noreply.github.com>
2024-07-12 10:54:03 -0300

    commit dos códigos da AVL

---
+++ Códigos AVL/loja de varejo.c
@@ -0,0 +1,41 @@
+#include "stdio.h"
+#include "stdlib.h"
+#include "string.h"
+#include "avl.h"
+
+typedef struct produto_do_estoque{
+
+int product_id;
+char name[100];
+char category[60];
+int quantity;
+double price;
+
+
+}product;
+
+
+void insert(product *produto){
+
+};
+
+
+product* search(int id){
+
+
+};
+
+update_quantity(int id, int delta){
+
+
+};
+
+void delete(int id){
+
+};
+
+
+int main(int argc, char const *argv[])
+{
+    
+}
