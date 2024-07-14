91597a9615979837d9f8a4b1977fc542884b57f7
assisgb <gabrielassisdev@gmail.com>
2024-07-14 01:46:11 -0300

    commit da edição de funções

--- Códigos AVL/loja de varejo.c
+++ Códigos AVL/loja de varejo.c
@@ -14,14 +14,47 @@ double price;
 
 }product;
 
+product* criar_produto(int id, char *nome, char *categoria, double preco, int quantidade){
+    product* novo = malloc(sizeof(product));
+    novo->product_id = id;
+    strcpy(novo->name, nome);
+    strcpy(novo->category, categoria);
+    novo->price = preco;
+    novo->quantity = quantidade;
 
-void insert(product *produto){
+    return novo;
+}
+
+void imprimir_produto(product *pd){
+    if (pd){
+        printf("ID: %d\nCATEGORIA: %s\nNOME: %s\nPREÇO: %.3lf\nQUANTIDADE: %d \n", pd->product_id, pd->category, pd->name, pd->price, pd->quantity);
+    }
+}
+
+int comparar_produto(product* pd1, product* pd2){
+    return (pd1->product_id - pd2->product_id);
+}
+
+
+void insert(t_avl *estoque ,product *produto){ 
+
+    int product_id, quantidade;
+    char nome[60];
+    char categoria[60];
+    double preco;
+    scanf("ID: %d", &product_id);
+    scanf("NOME: %s", nome);
+    scanf("CATEGORIA: %s", categoria);
+    scanf("PREÇO: %lf", &preco);
+    scanf("QUANTIDADE: %d", &quantidade);
+    produto = criar_produto(product_id, nome, categoria,preco,quantidade);
+    inserir_avl(estoque, produto);
+    
 
 };
 
 
-product* search(int id){
-
+product* search(t_avl *estoque,int id){
 
 };
 
@@ -36,6 +69,10 @@ void delete(int id){
 
 
 int main(int argc, char const *argv[])
-{
-    
+{    
+t_avl* estoque = criar_avl(imprimir_produto,comparar_produto);
+
+
+
+
 }
